a3e8e6838d20c30f6380d6d93081ee8c7aa83437
Lelyak-Andriy <a.lelyak47@gmail.com>
2022-06-26 16:41:47 +0300

    done

---
+++ controllers/auth/signup.js
@@ -0,0 +1,46 @@
+const { User } = require("../../models");
+const { Conflict } = require("http-errors");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const uuid = require("uuid");
+const { sendEmail } = require("../../helpers");
+
+const signup = async (req, res) => {
+  const { email, password, subscription } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw new Conflict(`User with ${email} already exist`);
+  }
+  const verificationToken = uuid.v4();
+  const avatarURL = gravatar.url(email);
+  const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  const result = await User.create({
+    email,
+    password: hashPassword,
+    subscription,
+    avatarURL,
+    verificationToken,
+  });
+
+  const mail = {
+    to: email,
+    subject: "Email verification",
+    html: `<a target="_blank" href="http://localhost:3000/api/users/verify/${verificationToken}">Verify your email</a>`,
+  };
+  await sendEmail(mail);
+
+  res.status(201).json({
+    status: "success",
+    code: 201,
+    data: {
+      user: {
+        email,
+        subscription,
+        avatarURL,
+        verificationToken,
+      },
+    },
+  });
+};
+
+module.exports = signup;
---
+++ controllers/users/index.js
@@ -0,0 +1,6 @@
+const getCurrent = require("./getCurrent");
+const updateAvatar = require("./updateAvatar");
+const verifyEmail = require("./verifyEmail");
+const emailReverify = require("./emailReverify");
+
+module.exports = { getCurrent, updateAvatar, verifyEmail, emailReverify };
